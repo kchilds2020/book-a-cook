21eae280ed999dd02e23defa43f3829c5aa4bc5a
kchilds2020 <63356648+kchilds2020@users.noreply.github.com>
2020-07-19 20:49:51 -0500

    changed the email sending from nodemailer to aws-sdk

--- apis/completeOrder.js
+++ apis/completeOrder.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const User = require('../models/User');
 const Orders = require('../models/Orders')
 const nodemailer = require("nodemailer");
+const AWS = require("aws-sdk");
 
 
 router.post('/api/post/complete-order', async(req, res) => {
@@ -10,8 +11,95 @@ router.post('/api/post/complete-order', async(req, res) => {
     let orderItem = await Orders.findOne({_id: req.body.orderID})
     let customer = await User.findOne({username: orderItem.customerUsername})
 
+    AWS.config.update({
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_ACCESS_SECRET_KEY,
+        region: 'us-east-1'
+    });
 
-    let transporter = nodemailer.createTransport({
+    const ses = new AWS.SES();
+        const customerParams = {
+        Destination: {
+            ToAddresses: ["lookforcooks@gmail.com"] // change for prod: `${customer.email}`
+        },
+        ConfigurationSetName: 'LFCconfig',
+        Message: {
+            Body: {
+                Html: {
+                    // HTML Format of the email
+                    Charset: "UTF-8",
+                    Data:
+                    `<html>
+                    <head>
+                        <style>
+                            *{
+                                color: black;
+                            }
+                            .username{
+                                color: white;
+                                background-color: #333;
+                                box-shadow: 0px 0px 4px #333;
+                                text-transform: uppercase;
+                                text-align: center;
+                            }
+                            .email-container{
+                                width: 100%;
+                                max-width: 600px;
+                                height: 600px;
+                                box-shadow: 0px 0px 4px #333;
+                                color: black;
+                                font-size: 18px;
+                                text-align: center;
+                            }
+                            .order{
+                                border: #333 1px sold;
+                            }
+                            .num{
+                                color: rgb(97, 169, 202);
+                            }
+
+                            .initial{
+                                font-size: 24px;
+                                font-weight: bold;
+                            }
+                            .disclaimer{
+                                font-size: 12px;
+                            }
+                        </style>
+                    </head>
+                    <body>
+                        <div class="email-container">
+                            <h2 class="username">${orderItem.menuItemTitle} has been Delivered ✔</h2>
+                            <div class = "information">
+                                <div class = "initial">Your cook has confirmed that the delivery has been completed.</div>
+                            </div>
+                            <div class="order">
+                                <div><b>Order ID:</b> ${orderItem._id}</div>
+                                <div><b>Deliver to:</b> ${orderItem.address}</div>
+                                <h3>Thank you for using Look for Cooks!</h3>
+                                <div class="disclaimer">If you have not received your food, you have one hour to dispute the delivery on our website. If we do not hear back by then, we will automatically mark the delivery as delivered and there will be no refunds.</div>
+                            </div>
+                        </div>
+                    </body>
+                </html>`
+                }
+            },
+            Subject: {
+                Charset: "UTF-8",
+                Data: `${orderItem.menuItemTitle} has been Delviered ✔`
+            }
+        },
+        Source: "lookforcooks@gmail.com"
+        };
+
+        const sendCustomerEmail = ses.sendEmail(customerParams).promise();
+
+        try{
+            const customerEmaildata = await sendCustomerEmail
+            console.log("email submitted to customer and SES", customerEmaildata);
+        }catch(error){ console.log(error) }
+
+    /* let transporter = nodemailer.createTransport({
         service: "gmail",
         auth: {
           user: 'lookforcooks@gmail.com', // generated ethereal user
@@ -79,7 +167,7 @@ router.post('/api/post/complete-order', async(req, res) => {
                 </html>
                 `, // html body
       });
-
+ */
       let updateOrder = await Orders.updateOne({_id: req.body.orderID},{
         $set: {
             completed: true,
--- apis/emailConfirmation.js
+++ apis/emailConfirmation.js
@@ -5,6 +5,7 @@ const JobPost = require('../models/JobPost');
 const Menu = require('../models/Menu');
 const Orders = require('../models/Orders')
 const nodemailer = require("nodemailer");
+const AWS = require("aws-sdk");
 
 require('dotenv/config');
 
@@ -25,7 +26,180 @@ router.post('/api/post/create-order', async (req, res) => {
 
         const cookInfo = await User.findOne({username: req.body.chefUsername})
 
-        let transporter = nodemailer.createTransport({
+        
+
+
+        AWS.config.update({
+            accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+            secretAccessKey: process.env.AWS_ACCESS_SECRET_KEY,
+            region: 'us-east-1'
+        });
+
+        const ses = new AWS.SES();
+        const customerParams = {
+        Destination: {
+            ToAddresses: ["lookforcooks@gmail.com"] // change for prod: `${req.body.customerEmail}`
+        },
+        ConfigurationSetName: 'LFCconfig',
+        Message: {
+            Body: {
+                Html: {
+                    // HTML Format of the email
+                    Charset: "UTF-8",
+                    Data:
+                    `<html>
+                        <head>
+                            <style>
+                                *{
+                                    color: black;
+                                }
+                                .username{
+                                    color: white;
+                                    background-color: #333;
+                                    box-shadow: 0px 0px 4px #333;
+                                    text-transform: uppercase;
+                                    text-align: center;
+                                }
+                                .email-container{
+                                    width: 100%;
+                                    max-width: 600px;
+                                    height: 600px;
+                                    box-shadow: 0px 0px 4px #333;
+                                    color: black;
+                                    font-size: 18px;
+                                    text-align: center;
+                                }
+                                .order{
+                                    border: #333 1px sold;
+                                }
+                                .num{
+                                    color: rgb(97, 169, 202);
+                                }
+
+                                .initial{
+                                    font-size: 24px;
+                                    font-weight: bold;
+                                }
+                                .disclaimer{
+                                    font-size: 12px;
+                                }
+                            </style>
+                        </head>
+                        <body>
+                            <div class="email-container">
+                                <h2 class="username">${req.body.customerUsername}</h2>
+                                <div class = "information">
+                                    <div class = "initial">Your order for <span class="num">${req.body.qty}</span> ${req.body.menuItemTitle}(s) has been placed!</div>
+                                </div>
+                                <div class="order">
+                                    <div><b>Order ID:</b> ${orderRes._id}</div>
+                                    <div><b>Deliver to:</b> ${req.body.address}</div>
+                                    <h3>Thank you for using Look for Cooks!</h3>
+                                    <div class="disclaimer">Please coordinate with your cook on delivery time. If your cook does not respond within one hour of your order, then you will be refunded in full.</div>
+                                </div>
+                            </div>
+                        </body>
+                    </html>`
+                }
+            },
+            Subject: {
+                Charset: "UTF-8",
+                Data: "Order has been Placed ✔"
+            }
+        },
+        Source: "lookforcooks@gmail.com"
+        };
+
+        const sendCustomerEmail = ses.sendEmail(customerParams).promise();
+
+        try{
+            const customerEmaildata = await sendCustomerEmail
+            console.log("email submitted to customer and SES", customerEmaildata);
+        }catch(error){ console.log(error) }
+
+        const cookParams = {
+            Destination: {
+                ToAddresses: ["lookforcooks@gmail.com"] // change for prod: `${req.body.customerEmail}`
+            },
+            ConfigurationSetName: 'LFCconfig',
+            Message: {
+                Body: {
+                    Html: {
+                        // HTML Format of the email
+                        Charset: "UTF-8",
+                        Data:
+                        `<html>
+                        <head>
+                            <style>
+                                *{
+                                    color: black;
+                                }
+                                .username{
+                                    color: white;
+                                    background-color: #333;
+                                    box-shadow: 0px 0px 4px #333;
+                                    text-transform: uppercase;
+                                    text-align: center;
+                                }
+                                .email-container{
+                                    width: 100%;
+                                    max-width: 600px;
+                                    height: 600px;
+                                    box-shadow: 0px 0px 4px #333;
+                                    color: black;
+                                    font-size: 18px;
+                                    text-align: center;
+                                }
+                                .order{
+                                    border: #333 1px sold;
+                                }
+                                .num{
+                                    color: rgb(97, 169, 202);
+                                }
+
+                                .initial{
+                                    font-size: 24px;
+                                    font-weight: bold;
+                                }
+                                .disclaimer{
+                                    font-size: 12px;
+                                }
+                            </style>
+                        </head>
+                        <body>
+                            <div class="email-container">
+                                <h2 class="username">${req.body.menuItemTitle}</h2>
+                                <div class = "information">
+                                    <div class = "initial">Quantity: <span class="num">${req.body.qty}</span></div>
+                                    <div><b>Customer Name:</b> ${req.body.user.firstName} ${req.body.user.lastName}</div>
+                                    <div><b>Order ID:</b> ${orderRes._id}</div>
+                                    <div><b>Deliver to:</b> ${req.body.address}</div
+                                </div>
+                                <div class="order">
+                                    <h3>Thank you for using Look for Cooks!</h3>
+                                    <div class="disclaimer">Please coordinate with your customer on delivery time.</div>
+                                </div>
+                            </div>
+                        </body>
+                    </html>`
+                    }
+                },
+                Subject: {
+                    Charset: "UTF-8",
+                    Data: "Order Up!"
+                }
+            },
+            Source: "lookforcooks@gmail.com"
+            };
+
+            const sendCookEmail = ses.sendEmail(customerParams).promise();
+
+        try{
+            const cookEmailData = await sendCookEmail
+            console.log("email submitted to Cook and SES", cookEmailData);
+        }catch(error){ console.log(error) }
+
+       /*  let transporter = nodemailer.createTransport({
             service: "gmail",
             auth: {
               user: 'lookforcooks@gmail.com', // generated ethereal user
@@ -155,7 +329,7 @@ router.post('/api/post/create-order', async (req, res) => {
                     `, // html body
           });
 
-          console.log("Message sent: %s", info.messageId, cookEmail.messageId);
+          console.log("Message sent: %s", info.messageId, cookEmail.messageId); */
         // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
 
